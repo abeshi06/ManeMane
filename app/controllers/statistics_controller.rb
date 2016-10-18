1e2271391333fc32832ed99e15e26bd60f365051
jhsAbe <abeshi06@hotmail.co.jp>
2016-10-18 17:40:53 +0900

    統計：円グラフ追加

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
   def circle_graph
+  	@sum_money = Money.where(loginuser: session[:usr]).includes(:category).group(:categoryname).sum(:price)
   end
 end
--- app/models/money.rb
+++ app/models/money.rb
@@ -1,3 +1,4 @@
 class Money < ActiveRecord::Base
   belongs_to :loginuser
+  belongs_to :category
 end
